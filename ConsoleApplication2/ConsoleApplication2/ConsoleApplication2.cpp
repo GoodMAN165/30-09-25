725594d63b7795989a204f66ff33969d01089e97
GoodMAN165 <2211doge1122@gmail.com>
2025-09-30 23:54:22 +0900

    +

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int randomnum = rand() % 500 + 1;
+    int usernum;
+    int attempt = 0;
+
+    cout << "Угадайте число\n";
+
+    cout << "Введите ваше число: "; cin >> usernum; 
+    
+    
+        while (usernum != randomnum) {
+        switch (usernum) {
+        case 0:
+            cout << "Игра закончена!";
+            return 0;
+        default:
+            if (usernum < randomnum) {
+                cout << "Ваше число меньше заданого\n";
+                cout << "Введите ваше число: "; cin >> usernum;
+                attempt++;
+            }
+            else {
+                cout << "Ваше число больше заданого\n";
+                cout << "Введите ваше число: "; cin >> usernum;
+                attempt++;
+            }
+        }
+    }
+    
+    
+    cout << "Вы победили!\n" << "Кол-во попыток: " << attempt;
+
+    return 0;
+}
---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    short userInput;
+    double ValValue;
+    cout << "Таблица доступных валют:\n USD - 1\n RUB - 2\n EUR - 3\n Ввод другого числа означает выход\n" << "Введите число: "; cin >> userInput;
+    cout << "Введите кол-во выбранной валюты: "; cin >> ValValue;
+    
+    while (userInput != 4) {
+        switch (userInput)
+        {
+        case 1:
+            cout << "RUB: " << ValValue * 83 << "\n EUR: " << ValValue / 0.8 << endl;
+            cout << "Введите число: "; cin >> userInput;
+            cout << "Введите кол-во выбранной валюты: "; cin >> ValValue;
+            break;
+        case 2:
+            cout << "USD: " << ValValue / 83 << "\n EUR: " << ValValue / 0.01 << endl;
+            cout << "Введите число: "; cin >> userInput;
+            cout << "Введите кол-во выбранной валюты: "; cin >> ValValue;
+            break;
+        case 3:
+            cout << "USD: " << ValValue * 0.8 << "\n RUB: " << ValValue * 0.01 << endl;
+            cout << "Введите число: "; cin >> userInput;
+            cout << "Введите кол-во выбранной валюты: "; cin >> ValValue;
+            break;
+        }
+    }
+    return 0;
+}
+
